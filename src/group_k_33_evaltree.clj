d481cd9fb1bc070e2d082ecf924b1d03b260aa15
Peter Mcteague <07mcteaguepet@googlemail.com>
2017-02-28 15:02:56 +0000

    Initial testing

---
+++ src/group_k_33_evaltree.clj
@@ -0,0 +1,12 @@
+ (ns group-k-33-evaltree)
+
+ (defn evaltree
+   ([list]
+    (evaltree (first list) (first (rest list)) (first (rest (rest list))))
+     )
+   ([exp one two]
+     ;;Easy case, one and two are both numbers
+    (if (and (number? one) (number? two))
+        (cons (apply (read-string exp) (cons one (cons two ()))) (cons one (cons two '())))
+      ))
+   )
