966600bbf08aebda60ad2ca3efc8db38969307dc
Peter Mcteague <07mcteaguepet@googlemail.com>
2017-03-04 18:10:28 +0000

    Fixed solution not handling '(+ (+ () 1) (+ 1 2))
    This obviously cannot be solved, needed to change ifs

--- src/group_k_33_evaltree.clj
+++ src/group_k_33_evaltree.clj
@@ -1,28 +1,47 @@
  (ns group-k-33-evaltree)
 
+;;A function for finding if a list contains an item (contains? was deprecated in an earlier version).
+;;Taken from http://stackoverflow.com/a/3249777
+(defn in?
+  "true if coll contains elm"
+  [coll elm]
+  (some #(= elm %) coll))
+
 ;;Basic recursive solution
 (defn evaltree-recursive
   ;;If we get a valid list, make a recursive call with the parts of the list as the arguments.
   ([list]
-    (if (= 3 (count list))
+   (if (and (= 3 (count list)) (not (in? list '())) )
       (evaltree-recursive (first list) (first (rest list)) (first (rest (rest list))))
-      "The list entered is invalid"
+      list
       ))
   ;;If we've got in an (exponent argument1 argument2) list
   ([exp one two]
    (cond
      ;;If both inputs are numbers, then apply the expression to one and two, and create a new list with the result replacing the expression.
      (and (number? one) (number? two))
-          (cons (apply (resolve(symbol exp)) (cons one (cons two '())))    (cons one (cons two '())))
+          (cons
+            (apply (resolve(symbol exp)) (cons one (cons two '())))
+            (cons one (cons two '())))
+
      ;;If the inputs are lists, then apply the expression to the solutions of both lists, and create a new list with the result replacing the expression.
-     (and (and (list? one) (list? two))  (and (= 3 (count one)) (= 3 (count two))))
-          (cons (apply (resolve(symbol exp)) (cons (first (evaltree-recursive one))  (cons (first (evaltree-recursive two)) ()))) (cons (evaltree-recursive one) (cons (evaltree-recursive two) '())))
+     (and (and (list? one) (list? two)) (and (not= one (evaltree-recursive one)) (not= two (evaltree-recursive two))))
+          (cons
+            (apply (resolve(symbol exp)) (cons (first (evaltree-recursive one))  (cons (first (evaltree-recursive two)) ())))
+            (cons (evaltree-recursive one) (cons (evaltree-recursive two) '())))
+
      ;;If just one is a list , solve one and apply the expression to the result of one and the value of two, before creating a new list.
-     (and (list? one) (= 3 (count one)))
-          (cons (apply (resolve(symbol exp)) (cons (first (evaltree-recursive one))  (cons two '()))) (cons (evaltree-recursive one) (cons two '())))
+     (and (number? two) (and (list? one) (not= one (evaltree-recursive-v2 one))))
+          (cons
+            (apply (resolve(symbol exp)) (cons (first (evaltree-recursive one))  (cons two '())))
+            (cons (evaltree-recursive one) (cons two '())))
+
      ;;If just two is a list , solve one and apply the expression to the result of two and the value of one, before creating a new list.
-     (and (list? two) (= 3 (count two)))
-          (cons (apply (resolve(symbol exp)) (cons one (cons (first (evaltree-recursive two)) '()))) (cons one (cons (evaltree-recursive two) '())))
+     (and (number? one) (and (list? two) (not= two (evaltree-recursive-v2 two))))
+          (cons
+            (apply (resolve(symbol exp)) (cons one (cons (first (evaltree-recursive two)) '())))
+            (cons one (cons (evaltree-recursive two) '())))
+
      ;;else just return the list with the expression.
      :else
           (cons exp (cons one (cons two '())))
@@ -31,34 +50,47 @@
   )
 
 ;;A slightly better solution using nth instead of first and rest and using conj instead of multiple cons's.
- (defn evaltree-recursive-v2
-   ;;If we get a valid list, make a recursive call with the parts of the list as the arguments.
-   ([list]
-     (if (= 3 (count list))
-       (evaltree-recursive-v2 (nth list 0) (nth list 1) (nth list 2))
-        "The list entered is invalid"
-       ))
-   ;;If we've got in an (exponent argument1 argument2) list
-   ([exp one two]
-     (cond
-       ;;If both inputs are numbers, then apply the expression to one and two, and create a new list with the result replacing the expression.
-       (and (number? one) (number? two))
-            (cons (apply (resolve(symbol exp)) (conj '() two one)) (conj '() two one))
-       ;;If the inputs are lists, then apply the expression to the solutions of both lists, and create a new list with the result replacing the expression.
-       (and (and (list? one) (list? two))  (and (= 3 (count one)) (= 3 (count two))))
-            (cons (apply (resolve(symbol exp)) (conj '() (first (evaltree-recursive-v2 two)) (first (evaltree-recursive-v2 one)))) (conj '() (evaltree-recursive-v2 two) (evaltree-recursive-v2 one)))
-       ;;If just one is a list , solve one and apply the expression to the result of one and the value of two, before creating a new list.
-       (and (list? one) (= 3 (count one)))
-            (cons (apply (resolve(symbol exp)) (conj '() two (first (evaltree-recursive-v2 one)))) (conj '() two (evaltree-recursive-v2 one)))
-       ;;If just two is a list , solve one and apply the expression to the result of two and the value of one, before creating a new list.
-       (and (list? two) (= 3 (count two)))
-            (cons (apply (resolve(symbol exp)) (conj '() (first (evaltree-recursive-v2 two)) one)) (conj '() (evaltree-recursive-v2 two) one))
-       ;;else just return the list with the expression.
-       :else
-            (conj '() two one exp)
-       )
-     )
-   )
+;;Also handles errors more cleanly.
+(defn evaltree-recursive-v2
+  ;;If we get a valid list, make a recursive call with the parts of the list as the arguments.
+  ([list]
+   (if (and (= 3 (count list)) (not (in? list '())))
+     (evaltree-recursive-v2 (nth list 0) (nth list 1) (nth list 2))
+     list
+     ))
+
+  ;;If we've got in an (exponent argument1 argument2) list
+  ([exp one two]
+   (cond
+     ;;If both inputs are numbers, then apply the expression to one and two, and create a new list with the result replacing the expression.
+     (and (number? one) (number? two))
+      (cons
+        (apply (resolve(symbol exp)) (conj '() two one))
+        (conj '() two one))
+
+     ;;If the inputs are lists, then apply the expression to the solutions of both lists, and create a new list with the result replacing the expression.
+     (and (and (list? one) (list? two)) (and (not= one (evaltree-recursive-v2 one)) (not= two (evaltree-recursive-v2 two))))
+      (cons
+        (apply (resolve(symbol exp)) (conj '() (first (evaltree-recursive-v2 two)) (first (evaltree-recursive-v2 one))))
+        (conj '() (evaltree-recursive-v2 two) (evaltree-recursive-v2 one)))
+
+     ;;If just one is a list , solve one and apply the expression to the result of one and the value of two, before creating a new list.
+     (and (number? two) (and (list? one) (not= one (evaltree-recursive-v2 one))))
+      (cons
+        (apply (resolve(symbol exp)) (conj '() two (first (evaltree-recursive-v2 one))))
+        (conj '() two (evaltree-recursive-v2 one)))
+
+     ;;If just two is a list , solve one and apply the expression to the result of two and the value of one, before creating a new list.
+     (and (number? one) (and (list? two) (not= two (evaltree-recursive-v2 two))))
+      (cons
+        (apply (resolve(symbol exp)) (conj '() (first (evaltree-recursive-v2 two)) one))
+        (conj '() (evaltree-recursive-v2 two) one))
+
+     ;;else just return the list with the expression.
+     :else
+      (conj '() two one exp)
+     )))
+
 
  ;;Tests for first recursive solution
  (evaltree-recursive '(* 3 2))
@@ -76,5 +108,7 @@
  (evaltree-recursive-v2 '(* () 2))
  (evaltree-recursive-v2 '(* 2))
  (evaltree-recursive-v2 '(* (+ 5 (* 3 7)) (- 6 8)))
+;;This following one however, causes issues as we can't verify whether or not
+  (evaltree-recursive-v2 '(* (+ 5 ()) (- 6 8)))             ;;broken one
 
- ;;Breaks if it's trying to solve a list containing an empty bracket.
+(and (and (list? '(+ 5 * ())) (list? '(- 6 8))) (and (not= '(+ 5 * ()) (evaltree-recursive-v2 '(+ 5 * ()))) (not= '(- 6 8) (evaltree-recursive-v2 '(- 6 8)))))
